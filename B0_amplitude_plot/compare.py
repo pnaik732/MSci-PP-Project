5e2bdcefbf37bcc6475c0007da426b05d7b6eab2
tz16559 <49797512+tz16559@users.noreply.github.com>
2020-03-30 18:22:13 +0100

    Add files via upload

--- B0_amplitude_plot/compare.py
+++ B0_amplitude_plot/compare.py
@@ -1,51 +1,60 @@
 import ROOT
 from ROOT import TFile, TCanvas, TPad, TLegend, TPaveText, TH2F, TMath
+import sys
 
-name = "Dbarpi"; number = "13"
+def main(progname, file_name, number):
+	myFile1 = TFile.Open("%s"%(file_name), "READ")
+	hist1   = myFile1.Get("Data_s%s"%(number))
 
-myFile1 = TFile.Open("Event_output.root", "READ")
-hist1   = myFile1.Get("Data_s%s"%(number))
+	myFile2 = TFile.Open("%s"%(file_name), "READ")
+	hist2   = myFile2.Get("Model_cat1_s%s"%(number))
 
-myFile2 = TFile.Open("Event_output.root", "READ")
-hist2   = myFile2.Get("Model_cat1_s%s"%(number))
+	c1 = TCanvas("c1", "fit", 1000,4000) 
+	hist1.Sumw2()
+	hist1.SetLineColor(ROOT.kOrange + 10)
+	hist1.SetMarkerStyle(20)
+	hist1.SetLineWidth(1)
+	hist1.SetMarkerSize(0.7)
+	hist1.SetMarkerColorAlpha(ROOT.kOrange + 10, 1);
+	# hist1.SetFillColorAlpha(ROOT.kOrange + 10, 0.4)
+	hist1.SetStats(False)
 
-c1 = TCanvas("c1", "fit", 550,500)  
-hist1.SetLineColor(ROOT.kWhite)
-hist1.SetFillColorAlpha(ROOT.kOrange + 10, 0.4)
-hist1.SetStats(False)
 
-hist2.SetLineColor(ROOT.kViolet + 10)
-hist2.SetLineWidth(2)
-hist2.SetLineStyle(1)
+	hist2.SetLineColor(ROOT.kViolet + 10)
+	hist2.SetLineStyle(1)
+	hist2.Sumw2()
+	hist2.SetMarkerStyle(20);
+	hist2.SetMarkerSize(0.7)
+	hist2.SetLineWidth(1);
+	hist2.SetMarkerColorAlpha(ROOT.kViolet + 10, 1);
+	hist2.SetStats(False)
 
-hist2.SetMarkerStyle(20);
-hist2.SetMarkerSize(0.5)
-hist2.SetLineWidth(1);
-hist2.SetMarkerColorAlpha(ROOT.kViolet + 10, 0.85);
-hist2.SetStats(False)
+	#Create pad1
+	pad1 = TPad("pad1", "", 0, 0, 1, 1)
+	pad1.SetLeftMargin(0.1)
+	pad1.SetRightMargin(0.1)
+	pad1.SetTopMargin(0.1)
+	pad1.SetBottomMargin(0.1)
+	pad1.Draw()
+	pad1.cd()
 
-#Create pad1
-pad1 = TPad("pad1", "", 0, 0, 1, 1)
-pad1.SetLeftMargin(0.1)
-pad1.SetRightMargin(0.1)
-pad1.SetTopMargin(0.1)
-pad1.SetBottomMargin(0.1)
-pad1.Draw()
-pad1.cd()
+	hist1.Draw("E1")
+	hist2.Draw("E1 same")
 
-hist1.Draw("B")
-hist2.Draw("L same")
+	#Add the legend
+	legend = TLegend(0.65, 0.75, 0.89, 0.89)
+	legend.AddEntry(hist1, "LHCb data", "lpe")
+	legend.AddEntry(hist2, "AmpGen fitter", "lpe")
 
-#Add the legend
-legend = TLegend(0.55, 0.65, 0.85, 0.85)
-legend.AddEntry(hist1, "LHCb data", "f")
-legend.AddEntry(hist2, "amplitude fit", "lpe")
+	legend.SetTextFont(133)
+	legend.SetTextSize(30)
+	legend.SetLineColor(0)
+	legend.Draw()
 
-legend.SetTextFont(133)
-legend.SetTextSize(20)
-legend.SetLineColor(0)
-legend.Draw()
-
-c1.SaveAs("compare%s.png"%(name))
-
-input()
+	c1.SaveAs("compare%s.png"%(number))
+	
+if __name__ == '__main__':
+    PROGNAME    =     sys.argv[0]
+    FILE_NAME   = str(sys.argv[1])
+    NUMBER      = str(sys.argv[2])      
+    main(PROGNAME,FILE_NAME,NUMBER)
---
+++ B0_amplitude_plot/run_compare.py
@@ -0,0 +1,39 @@
+import subprocess
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+def run_cmd(cmd):
+  process = subprocess.Popen(cmd.split(), stdout = subprocess.PIPE)
+  #process.communicate()
+  stdout = process.communicate()[0]
+  print ('STDOUT:{}'.format(stdout))
+  process.stdout.close()
+  
+def trim_axs(axs, N):
+    """little helper to massage the axs list to have correct length..."""
+    axs = axs.flat
+    for ax in axs[N:]:
+        ax.remove()
+    return axs[:N]
+
+# decay_model = "B0toDDbar0K+pi-_spd.opt"
+# data_file   = "Event_nondup.root"
+output_file = "Event_output_nondup_conj.root"
+# run_cmd("../SignalOnlyFitter %s --DataSample %s --Plots %s"%(decay_model,data_file,output_file))
+
+
+
+number = ["01","02","03","12","13","23","012","013","023","123"]
+for i in number:
+	run_cmd("python compare.py %s %s"%(output_file,i))
+ 	
+fig,axes = plt.subplots(3,4,figsize=(100,60))
+axs = trim_axs(axes, 10)
+
+for ax, no in zip(axs, number):
+#     ax.set_title('markevery=%s' % str(case))
+    ax.imshow(mpimg.imread("compare%s.png"%(no))); ax.set_axis_off()
+
+fig.tight_layout()
+fig.savefig("compare_all_conj.png")
